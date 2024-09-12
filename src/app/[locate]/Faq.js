d435236dbdaf882f5924ceb406454c91e8ad7259
Jesus Santiago <jesusantiago420@gmail.com>
2024-09-12 13:57:12 +0200

    COMPLETE section FAQ

--- src/app/[locate]/Faq.js
+++ src/app/[locate]/Faq.js
@@ -2,6 +2,7 @@ import { Accordion, AccordionDetails, AccordionSummary, Box, Card, CardMedia, Fa
 import ExpandMoreIcon from '@mui/icons-material/ExpandMoreRounded'
 import { useTranslations } from 'next-intl'
 import { useState } from 'react';
+import GitHub from '@mui/icons-material/GitHub';
 
 const Faq = () => {
     const t = useTranslations('Home.FAQ')
@@ -48,16 +49,17 @@ const Faq = () => {
                     maxWidth: '1620px',
                     display: 'flex',
                     flexDirection: 'column',
-                    gap: 4
+                    gap:4
                 }}
             >
 
                 <Accordion
+                    square
                     sx={{
                         boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
+                        borderRadius: 4,
                     }}
-                >
-
+                    >
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1-content"
@@ -66,27 +68,25 @@ const Faq = () => {
                         <Typography
                             variant='h5'
                         >
-                            ¿Que puedo hacer por ti?
+                            {t('FAQ1.title')}
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography
                             variant='body1'
                         >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            {t('FAQ1.question')}
                         </Typography>
                     </AccordionDetails>
-
-
                 </Accordion>
 
                 <Accordion
                     sx={{
+                        borderRadius: 4,
+                        backgroundColor: 'none',
                         boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
                     }}
-                >
-
+                    >
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1-content"
@@ -95,27 +95,25 @@ const Faq = () => {
                         <Typography
                             variant='h5'
                         >
-                            Custom transition using Fade
+                            {t('FAQ2.title')}
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography
                             variant='body1'
                         >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            {t('FAQ2.question')}
                         </Typography>
                     </AccordionDetails>
-
-
                 </Accordion>
 
                 <Accordion
+                    square
                     sx={{
+                        borderRadius: 4,
                         boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
                     }}
                 >
-
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1-content"
@@ -124,27 +122,25 @@ const Faq = () => {
                         <Typography
                             variant='h5'
                         >
-                            Custom transition using Fade
+                            {t('FAQ3.title')}
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography
                             variant='body1'
                         >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            {t('FAQ3.question')}
                         </Typography>
                     </AccordionDetails>
-
-
                 </Accordion>
 
                 <Accordion
+                    square
                     sx={{
                         boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
+                        borderRadius: 4
                     }}
                 >
-
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1-content"
@@ -153,27 +149,25 @@ const Faq = () => {
                         <Typography
                             variant='h5'
                         >
-                            Custom transition using Fade
+                            {t('FAQ4.title')}
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography
                             variant='body1'
                         >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            {t('FAQ4.question')}
                         </Typography>
                     </AccordionDetails>
-
-
                 </Accordion>
 
                 <Accordion
+                    square
                     sx={{
                         boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
+                        borderRadius: 4
                     }}
                 >
-
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1-content"
@@ -182,20 +176,99 @@ const Faq = () => {
                         <Typography
                             variant='h5'
                         >
-                            Custom transition using Fade
+                            {t('FAQ5.title')}
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography
                             variant='body1'
                         >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            {t('FAQ5.question')}
                         </Typography>
                     </AccordionDetails>
-
-
                 </Accordion>
+
+                <Accordion
+                    square
+                    sx={{
+                        boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
+                        borderRadius: 4,
+                    }}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1-content"
+                        id="panel1-header"
+                    >
+                        <Typography
+                            variant='h5'
+                        >
+                            {t('FAQ6.title')}
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography
+                            variant='body1'
+                        >
+                            {t('FAQ6.question')}
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion
+                    square
+                    sx={{
+                        boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
+                        borderRadius: 4
+                    }}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1-content"
+                        id="panel1-header"
+                    >
+                        <Typography
+                            variant='h5'
+                        >
+                            {t('FAQ7.title')}
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography
+                            variant='body1'
+                        >
+                            {t('FAQ7.question')}
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
+                <Accordion
+                    square
+                    sx={{
+                        boxShadow: (theme) => `15px 15px 30px ${theme.palette.card.shadowPrimary}, -15px -15px 30px ${theme.palette.card.shadowSecondary}`,
+                        borderRadius: 4
+                    }}
+                >
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1-content"
+                        id="panel1-header"
+                    >
+                        <Typography
+                            variant='h5'
+                        >
+                            {t('FAQ8.title')}
+                        </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography
+                            variant='body1'
+                        >
+                           {t('FAQ8.question')}
+                        </Typography>
+                    </AccordionDetails>
+                </Accordion>
+
 
 
             </Box>
