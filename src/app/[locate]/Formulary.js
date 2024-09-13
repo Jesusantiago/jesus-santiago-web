4fc8b2953a4612464712a7663b72e78f046c15c5
Jesus Santiago <jesusantiago420@gmail.com>
2024-09-13 22:29:07 +0200

    ADD front-end formulary and react-hook-form

--- src/app/[locate]/Formulary.js
+++ src/app/[locate]/Formulary.js
@@ -1,16 +1,22 @@
 import { LinkRounded } from "@mui/icons-material"
 import GitHub from "@mui/icons-material/GitHub"
-import { Box, Button, Card, CardContent, CardMedia, Chip, Divider, Typography } from "@mui/material"
+import { Box, Button, Card, CardContent, CardMedia, Chip, Divider, TextField, Typography } from "@mui/material"
 import Grid from '@mui/material/Unstable_Grid2'
 import { useTranslations } from 'next-intl'
 import { Link } from "@/i18n/routing";
+import { useForm } from 'react-hook-form'
 
 
 
 const Formulary = () => {
 
     const t = useTranslations('Home.formulary')
+    const { register, handleSubmit, formState: {errors} } = useForm()
 
+    const OnSubmit = (data) => {
+        console.log(data)
+    }
+ 
     return (
         <Box
             component='section'
@@ -44,7 +50,54 @@ const Formulary = () => {
                 {t('title')}
             </Typography>
 
-            
+            <Box
+                component='form'
+                onSubmit={handleSubmit(OnSubmit)}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between',
+                    flexWrap : 'wrap',
+                    gap: 2
+                }}
+            >
+
+                <TextField 
+                    label={t('name')}
+                    variant="filled"
+                    sx={{
+                        width: .4,
+
+                    }}
+                    {...register('name')}
+                />
+
+                <TextField 
+                    label={t('email')}
+                    variant="filled"
+                    sx={{
+                        width: .4
+                    }}
+                    {...register('email')}
+
+                />
+                
+                <TextField 
+                    label={t('text')}
+                    variant="filled"
+                    sx={{
+                        width: 1
+                    }}
+                    {...register('text')}
+                />
+
+                <Button
+                    variant="contained"
+                    type="submit"
+                > Hola 
+                
+                </Button>
+            </Box>
 
         </Box>
     )
